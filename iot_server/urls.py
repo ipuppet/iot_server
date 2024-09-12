eefc80ce1c83243f4af2e7bad60d46ad55830aea
Samuel <zimingw.ol@outlook.com>
2024-09-12 18:52:38 +0800

    only use swagger-ui

--- iot_server/urls.py
+++ iot_server/urls.py
@@ -1,15 +1,16 @@
 from django.contrib import admin
 from django.urls import include, path
-from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+from drf_spectacular.views import SpectacularSwaggerView
 
 urlpatterns = [
-    # YOUR PATTERNS
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    # Optional UI:
-    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),  # swagger接口文档
-    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),  # redoc接口文档
-
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+    # swagger-ui
+    path(
+        "api-schema/swagger-ui/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    # App URLs
     path("automation/", include("automation.urls")),
 ]
