586ce7666e43066a73043948b40fe4537528bdec
Samuel <zimingw.ol@outlook.com>
2024-09-12 19:00:46 +0800

    fix: swagger-ui

--- iot_server/urls.py
+++ iot_server/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
     # swagger-ui
-    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
         "schema/swagger-ui/",
         SpectacularSwaggerView.as_view(url_name="schema"),
