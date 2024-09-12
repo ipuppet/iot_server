260d7a61196e572c7a522c7bef467870fd4dd63d
Samuel <zimingw.ol@outlook.com>
2024-09-12 18:34:08 +0800

    add drf-spectacular

--- build.sh
+++ build.sh
@@ -10,4 +10,4 @@ image_name="cits5506-server"
 docker container stop $image_name
 docker container rm $image_name
 docker build -t $image_name .
-docker run -d -p 8090:8000 --name $image_name $image_name
+docker run -d -p 8090:8000 -v $WORK_DIR/db.sqlite3:/app/db.sqlite3 --name $image_name $image_name
