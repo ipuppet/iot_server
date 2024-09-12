31a9d80f8a84e3bc655d155280f47879b86825cc
Samuel <zimingw.ol@outlook.com>
2024-09-12 18:59:51 +0800

    fix: swagger-ui

--- iot_server/urls.py
+++ iot_server/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import include, path
-from drf_spectacular.views import SpectacularSwaggerView
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
     # swagger-ui
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
         "schema/swagger-ui/",
         SpectacularSwaggerView.as_view(url_name="schema"),
