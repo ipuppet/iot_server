92b02c3515d50e1bec5d9240e0b61cf829877689
Samuel <zimingw.ol@outlook.com>
2024-09-26 19:04:56 +0800

    chore: Add reset option to build script

--- build.sh
+++ build.sh
@@ -11,8 +11,13 @@ db_file=$WORK_DIR/db.sqlite3
 docker container stop $image_name
 docker container rm $image_name
 docker build -t $image_name .
+
+# if argument 1 is reset
+if [ "$1" == "reset" ]; then
+    rm -f $db_file
+fi
+
 if [ ! -f $db_file ]; then
     touch $db_file
 fi
 docker run -d -p 8090:8000 -v $db_file:/app/db.sqlite3 --name $image_name $image_name
-docker exec -t $image_name bash -c "/app/resetdb.sh"
